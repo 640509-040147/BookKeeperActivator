20cd18ed0cca6dd17aed2103a99817ef474e067b
vikram <vikram@ox.io>
2018-04-01 16:41:18 +0530

    Add project

---
+++ BookKeeperActivator/Activator.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace BookKeeperActivator
+{
+    public class Activator
+    {
+        public static string appSettingFilePath =
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\user.dat";
+
+        public void WriteActivationFile()
+        {
+            var now = DateAndTime.Now;
+            var dateTime = now;
+            var data2 = convertDateFromDisplayToSqliteFormat(dateTime.AddDays(365.0 * 5));
+            writeToExpiryDateFile(appSettingFilePath, false, DEVICE_IMEI());
+            writeToExpiryDateFile(appSettingFilePath, true, data2);
+        }
+
+        private static void writeToExpiryDateFile(string fileName, bool append, string data)
+        {
+            var simple3Des = new Simple3Des("fAa$BOoKkEepEr#");
+            var streamWriter = new StreamWriter(fileName, append);
+            try
+            {
+                streamWriter.WriteLine(simple3Des.EncryptData(data));
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                streamWriter.Close();
+            }
+        }
+
+
+        private string convertDateFromDisplayToSqliteFormat(DateTime displayDate)
+        {
+            return displayDate.ToString("yyyy-MM-dd");
+        }
+
+
+        private string convertToASCII(string str)
+        {
+            var text = "";
+            var num = 0;
+            checked
+            {
+                var num2 = str.Length - 1;
+                var num3 = num;
+                for (;;)
+                {
+                    var num4 = num3;
+                    var num5 = num2;
+                    if (num4 > num5) break;
+                    text += Convert.ToString(Strings.Asc(str[num3]));
+                    num3++;
+                }
+
+                return text;
+            }
+        }
+
+        private string DEVICE_IMEI()
+        {
+            var text = "";
+            bool flag;
+            try
+            {
+                var managementObjectSearcher =
+                    new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DiskDrive");
+                try
+                {
+                    var enumerator = managementObjectSearcher.Get().GetEnumerator();
+                    if (enumerator.MoveNext())
+                    {
+                        var managementObject = (ManagementObject) enumerator.Current;
+                        text = managementObject["SerialNumber"].ToString();
+                    }
+                }
+                finally
+                {
+                    ManagementObjectCollection.ManagementObjectEnumerator enumerator = null;
+                    flag = enumerator != null;
+                    if (flag) ((IDisposable) enumerator).Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                text = "";
+            }
+
+            flag = Operators.CompareString(text, "", false) == 0;
+            if (flag)
+            {
+                text += convertToASCII("bK");
+                text += convertToASCII(Environment.GetEnvironmentVariable("PROCESSOR_REVISION"));
+                text += convertToASCII(Strings.StrReverse(Environment.UserName));
+                text += convertToASCII(Environment.GetEnvironmentVariable("NUMBER_OF_PROCESSORS"));
+                text += convertToASCII(Strings.StrReverse(Environment.MachineName));
+            }
+
+            text = Strings.Trim(text);
+            return text;
+        }
+    }
+    ///////////////////////////////////////////////
+
+    public class Simple3Des
+    {
+        private readonly TripleDESCryptoServiceProvider TripleDes;
+
+        public Simple3Des(string key)
+        {
+            TripleDes = new TripleDESCryptoServiceProvider();
+            TripleDes.Key = TruncateHash(key, TripleDes.KeySize / 8);
+            TripleDes.IV = TruncateHash("", TripleDes.BlockSize / 8);
+        }
+
+        private byte[] TruncateHash(string key, int length)
+        {
+            var sha1CryptoServiceProvider = new SHA1CryptoServiceProvider();
+            var bytes = Encoding.Unicode.GetBytes(key);
+            var array = sha1CryptoServiceProvider.ComputeHash(bytes);
+            return (byte[]) Utils.CopyArray(array, new byte[checked(length - 1 + 1)]);
+        }
+
+        public string EncryptData(string plaintext)
+        {
+            var bytes = Encoding.Unicode.GetBytes(plaintext);
+            var memoryStream = new MemoryStream();
+            var cryptoStream = new CryptoStream(memoryStream, TripleDes.CreateEncryptor(), CryptoStreamMode.Write);
+            cryptoStream.Write(bytes, 0, bytes.Length);
+            cryptoStream.FlushFinalBlock();
+            return Convert.ToBase64String(memoryStream.ToArray());
+        }
+    }
+}
---
+++ BookKeeperActivator/ConsoleApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace BookKeeperActivator
+{
+    public class ConsoleApp
+    {
+        public static void Main(string[] args)
+        {
+            var acto = new Activator();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "Book Keeper Activator";
+            const string title = @"
+    ╦ ╦╦═╗╔═╗╔╦╗
+    ║ ║╠╦╝║╣  ║ 
+    ╚═╝╩╚═╚═╝ ╩ 
+--------------------
+Book Keeper 5.6.3 Activator
+RCE by vikram
+";
+            Console.Write(title);
+            Thread.Sleep(500);
+            if (File.Exists(Activator.appSettingFilePath))
+            {
+                Console.WriteLine("Using activation file: {0}",Activator.appSettingFilePath);
+                Thread.Sleep(900);
+                Console.WriteLine("Writing activation data to file.");
+            }
+            else
+            {
+                Console.WriteLine("No activation file found");
+                Thread.Sleep(900);
+                Console.WriteLine("Creating new activation file.");
+
+            }
+
+            Console.WriteLine("Activating for 5 years.");
+            Thread.Sleep(900);
+            acto.WriteActivationFile();
+            using (var progress = new ProgressBar()) {
+                for (int i = 0; i <= 100; i++) {
+                    progress.Report((double) i / 100);
+                    Thread.Sleep(30);
+                }
+            }
+            Console.WriteLine("Congrats! your copy is now activated for 5 more years.");
+
+            
+            Console.Write("Enter any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
